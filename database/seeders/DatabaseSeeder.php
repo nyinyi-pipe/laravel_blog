f2dca026ad45f085123bdd81968d50572e0e6f49
nyinyi <nyinyi@pipe.bz>
2023-03-28 21:52:30 +0630

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Article;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Article::factory()->count(20)->create();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+Route::get('/', [ArticleController::class, 'index']);
+
+Route::get('/articles', [ArticleController::class, 'index']);
+
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
